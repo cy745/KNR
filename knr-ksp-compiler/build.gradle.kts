f45b20002929a6d7b84267fedaafcf389c47c128
cy745 <1248393538@qq.com>
2025-03-04 11:48:02 +0800

    feat(compiler): 添加 KNR编译器实现
    - 新增 KNR 编译器模块，包括代码生成、参数处理等功能- 实现了 KNRCollectProcessor 和 KNRInjectProcessor 两个处理器
    - 添加了参数状态类和路由映射生成代码
    -集成了 KotlinPoet 和 KSP 相关库

---
+++ knr-ksp-compiler/build.gradle.kts
@@ -0,0 +1,76 @@
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinJvm
+import com.vanniktech.maven.publish.SonatypeHost
+
+plugins {
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.vanniktech.publish)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+    implementation(project(":knr-core"))
+
+    // 用于测试ksp处理器
+    testImplementation("dev.zacsweers.kctfork:ksp:0.5.1")
+    testImplementation("org.jetbrains.kotlin:kotlin-compiler-embeddable")
+    testImplementation(libs.junit)
+
+    implementation(libs.ksp.api)
+    implementation("com.squareup:kotlinpoet:1.18.1")
+    implementation("com.squareup:kotlinpoet-ksp:1.18.1")
+}
+
+group = libs.versions.group.get()
+version = libs.versions.version.get()
+
+mavenPublishing {
+    coordinates(
+        groupId = group.toString(),
+        artifactId = "compiler",
+        version = version.toString()
+    )
+
+    configure(
+        KotlinJvm(
+            javadocJar = JavadocJar.Javadoc(),
+            sourcesJar = true,
+        )
+    )
+
+    pom {
+        name = "KNR Compiler"
+        description = "compiler module of KNR"
+        inceptionYear = "2024"
+        url = "https://github.com/cy745/knr/"
+
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+
+        developers {
+            developer {
+                id = "cy745"
+                name = "cy745"
+                url = "https://github.com/cy745/"
+            }
+        }
+
+        scm {
+            url = "https://github.com/cy745/knr/"
+            connection = "scm:git:git://github.com/cy745/knr.git"
+            developerConnection = "scm:git:ssh://git@github.com/cy745/knr.git"
+        }
+    }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+}
